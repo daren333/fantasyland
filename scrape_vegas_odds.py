7024bb5d8111b15c0756b1e6bebd926775af813b
Daren Ansher <dansher@Darens-MacBook-Pro.local>
2019-08-25 17:50:39 -0400

    updated vegas odds scraper

--- scrape_vegas_odds.py
+++ scrape_vegas_odds.py
@@ -1,18 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 url = 'http://www.vegasinsider.com/nfl/odds/las-vegas/?s=19'
 html = requests.get(url)
 
 
-soup = BeautifulSoup(html.text)
+soup = BeautifulSoup(html.text, features='lxml')
 full_tab = soup.find("table", class_="frodds-data-tbl")
 
-for c in full_tab.contents:
-	print("new content: %s"%c)
-	#print(row)
-#tab = full_tab[0].findall("tr")
+rows = []
+for row in full_tab.contents:
+	rowstr = str(row)
+	if re.search("br(.*?)\n", rowstr):
+		rows.append(rowstr)
+	#print(str)
+spreads = []
+over_unders = []
+for row in rows:
+	m = re.search("<br/>(.?\d+½?(o|u)?).*?<br/>(.?\d+½?(o|u)?).*?", row)
+	if m:
+		if re.search('[ou]', m.group(1)):
+			over_unders.append(m.group(1))
+			spreads.append(m.group(3))
+		else:
+			spreads.append(m.group(1))
+			over_unders.append(m.group(3))
+		#print(m.group(1))
+for spread in spreads:
+	print('spread: %s' %spread)
 
-#for game in tab.
-#tab = full_tab.split("<\\td>")
-#print(tab[1])
+for ou in over_unders:
+	print('Over/Under: %s' %ou)
