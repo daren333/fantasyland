d5bf3ed894dbe215e357c82663d43504ca26b093
Daren Ansher <dansher@Darens-MacBook-Pro.local>
2019-08-25 21:43:08 -0400

    finished vegas scraper

--- scrape_vegas_odds.py
+++ scrape_vegas_odds.py
@@ -2,6 +2,27 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+
+class Game:
+
+	def __init__(self, date=None, home=None, away=None, favorite=None, spread=None, over_under=None):
+		self.date = date
+		self.home = home
+		self.away = away
+		self.favorite = favorite
+		self.spread = spread
+		self.over_under = over_under
+
+	def to_string(self):
+		print("date: %s" %self.date)
+		print("home: %s" %self.home)
+		print("away: %s" %self.away)
+		print("favorite: %s" %self.favorite)
+		print("spread: %s" %self.spread)
+		print("over_under: %s" %self.over_under)
+
+
+
 url = 'http://www.vegasinsider.com/nfl/odds/las-vegas/?s=19'
 html = requests.get(url)
 
@@ -14,21 +35,30 @@ for row in full_tab.contents:
 	rowstr = str(row)
 	if re.search("br(.*?)\n", rowstr):
 		rows.append(rowstr)
-	#print(str)
-spreads = []
-over_unders = []
+
+games = []
+
 for row in rows:
+	game = Game()
+	#print(row)
+	m = re.search("href.*?/([A-Za-z]*?)-@-([A-Za-z]*?)\.cfm/date/(\d{2}\-\d{2}\-\d{2})#", row)
+	if m:
+		game.away = m.group(1)
+		game.home = m.group(2)
+		game.date = m.group(3)
+
 	m = re.search("<br/>(.?\d+½?(o|u)?).*?<br/>(.?\d+½?(o|u)?).*?", row)
 	if m:
 		if re.search('[ou]', m.group(1)):
-			over_unders.append(m.group(1))
-			spreads.append(m.group(3))
+			game.over_under = m.group(1)
+			game.spread = m.group(3)
+			game.favorite = game.away
 		else:
-			spreads.append(m.group(1))
-			over_unders.append(m.group(3))
-		#print(m.group(1))
-for spread in spreads:
-	print('spread: %s' %spread)
+			game.spread = m.group(1)
+			game.over_under = m.group(3)
+			game.favorite = game.home
 
-for ou in over_unders:
-	print('Over/Under: %s' %ou)
+	games.append(game)
+
+for game in games:
+	game.to_string()
